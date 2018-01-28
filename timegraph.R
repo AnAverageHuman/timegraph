8b20ac77aef0c3e23b9407d3dbd7f640160734fe
Jeffrey Lin <jeffrey@icurse.nl>
2018-01-28 16:49:24 -0500

    timegraph.R: refactor
    - Move most processing to functions.
    
    - Parse arguments with argparse if we are not running interactively.
    
    - Remove some unnecessary arguments in call to ggplot().

--- timegraph.R
+++ timegraph.R
@@ -1,60 +1,64 @@
 #!/usr/bin/Rscript
+
+library(argparse)
 library(ggplot2)
 
-data <- read.csv(commandArgs(TRUE)[1])
-data$date  <- as.Date(data$date, format = '%m/%d')
-data$hours <- data$end - data$start
+parse_arguments <- function() {
+  parser <- ArgumentParser()
+  parser$add_argument("file", nargs = 1, help = "file with data")
+  args <- parser$parse_args()
 
-wAvg <- aggregate(data$hours, list(week = data$week), mean)
-data$wAvg <- rep(wAvg$x, table(data$week))
+  if (! file.exists(args$file)) {
+    stop("Data file ", args$file, " does not exist.", call. = FALSE)
+  }
 
-lastDay <- aggregate(date ~ week, data = data, max)
+  args
+}
+
+parse_csv <- function(filename) {
+  data <- read.csv(filename)
+  data$date <- as.Date(data$date, format = "%m/%d")
+  data$hours <- data$end - data$start
+  data
+}
+
+get_averages <- function(df) {
+  aggregate(df$hours, list(week = df$week), mean)
+}
 
 # generate floating bar graph using boxplots
-p <-
-  ggplot(data,
-         aes(x      = date,
-             y      = hours,
-             ymin   = start,
-             ymax   = end,
-             lower  = start,
-             upper  = end,
-             middle = start,
-             group  = week,
-             color  = hours > mean(hours))) +
-  theme(plot.title      = element_text(size   = 20,
-                                       face   = "bold",
-                                       margin = margin(10, 0, 10, 0),
-                                       hjust  = 0.5),
-        axis.text.x     = element_text(angle = 30,
-                                       vjust = 1,
-                                       hjust = 1),
-        legend.position = "bottom") +
-  geom_boxplot(stat = "identity")
+create_bargraph <- function(df) {
+  ggplot(df, aes(x = date,
+                 y = hours,
+                 ymin = start,
+                 ymax = end,
+                 lower = start,
+                 upper = end,
+                 middle = start,
+                 color = hours > mean(hours))) +
+    theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1),
+          legend.position = "bottom") +
+    geom_boxplot(stat = "identity")
+}
 
 # generate hours scatterplot
-q <-
-  ggplot(data, aes(x     = date,
-                   y     = hours,
-                   color = factor(week))) +
-
-  theme(axis.text.x = element_text(angle = 30,
-                                   vjust = 1,
-                                   hjust = 1)) +
+create_scatterplot <- function(df, w_avg) {
+  ggplot(df, aes(x = date, y = hours, color = factor(week))) +
+  theme(axis.text.x = element_text(angle = 30, vjust = 1, hjust = 1)) +
   geom_point() +
-  geom_smooth(inherit.aes = FALSE,
-              method      = "auto",
-              se          = FALSE,
-              aes(x       = data$date,
-                  y       = data$hours)) +
-  geom_hline(aes(color      = factor(week),
-                 yintercept = data$wAvg)) +
-  geom_hline(aes(color      = "purple",
-                 yintercept = mean(data$hours))) +
+  geom_smooth(inherit.aes = FALSE, se = FALSE, aes(x = date, y = hours)) +
+  geom_hline(aes(color = factor(week), yintercept = df$w_avg)) +
+  geom_hline(aes(color = "black", yintercept = mean(df$hours))) +
   scale_color_discrete(name   = "Average",
-                       labels = c(paste("Week", wAvg$week), "Total")) +
+                       labels = c(paste("Week", w_avg$week), "Total")) +
   theme(legend.position = "bottom")
+}
 
-
-ggsave("bars.png", plot = p)
-ggsave("averages.png", plot = q)
+if (! interactive()) {
+  args <- parse_arguments()
+  df <- parse_csv(args$file)
+  w_avg <- get_averages(df)
+  df$w_avg <- rep(w_avg$x, table(df$week))
+  ggsave("bars.png", plot = create_bargraph(df))
+  ggsave("averages.png", plot = create_scatterplot(df, w_avg))
+}
